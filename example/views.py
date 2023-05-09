c8397a8790ddcb9a8ca8eb7069260b1f161f0d7a
bonitoFotso <bonitofotso55@gmail.com>
2023-05-09 06:09:15 +0100

    pages

--- example/views.py
+++ example/views.py
@@ -1,16 +1,10 @@
 # example/views.py
 from datetime import datetime
+from django.shortcuts import render
 
 from django.http import HttpResponse
 
+
+
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+  return render(request, 'pages/index.html')
